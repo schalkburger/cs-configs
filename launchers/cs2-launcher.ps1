7b64203098de75801fd7f1638166754c9bc65f61
Schalk Burger <schalkb@gmail.com>
2024-03-03 08:47:10 +0200

    Warmup and network tweaks

--- launchers/cs2-launcher.ps1
+++ launchers/cs2-launcher.ps1
@@ -17,24 +17,23 @@ try {
         # Write-Host "Press any key to derank...";
         # $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
         # Start CSGO
-        SetScreenResolution 1920 1080
-        Start-Sleep 2
+        # SetScreenResolution 1920 1080
+        # Start-Sleep 2
         Start-Process steam://launch/730/Dialog
-        New-BurntToastNotification -AppLogo "E:\Apps\Windows Apps\Custom Icons\cs2.png" -Text "Launching Counter-Strike 2...", 'Counter-Strike 2'
+        # New-BurntToastNotification -AppLogo "E:\Apps\Windows Apps\Custom Icons\cs2.png" -Text "Launching Counter-Strike 2...", 'Counter-Strike 2'
         # MultiMonitorTool /LoadConfig "E:\GitHub\cs-configs\launchers\monitors-cs2-1280x960.cfg"
-        Write-Host "`n";
+        # Write-Host "`n";
         # Notepad wait
-        Start-Process -WindowStyle "Minimized" -Wait notepad
+        # Start-Process -WindowStyle "Minimized" -Wait notepad
         # Start-Process -WindowStyle "Minimized" -Wait "C:\Program Files\WindowsApps\Microsoft.WindowsAlarms_11.2309.5.0_x64__8wekyb3d8bbwe\Time.exe"
         # Stop CSGO
-        Stop-Process -processname "cs2"
-        SetScreenResolution 1920 1080
-        Start-Sleep 2
-        MultiMonitorTool /LoadConfig "E:\Apps\Windows Apps\MultiMonitorTool\monitors-default.cfg"
-        New-BurntToastNotification -AppLogo "E:\Apps\Windows Apps\Custom Icons\cs2.png" -Text "Stopped playing CS2.",
-        'Counter-Strike 2'
+        # Stop-Process -processname "cs2"
+        # SetScreenResolution 1920 1080
+        # Start-Sleep 2
+        # MultiMonitorTool /LoadConfig "E:\Apps\Windows Apps\MultiMonitorTool\monitors-default.cfg"
+        # New-BurntToastNotification -AppLogo "E:\Apps\Windows Apps\Custom Icons\cs2.png" -Text "Stopped playing CS2.", 'Counter-Strike 2'
         # Close terminal
-        Start-Sleep 1
+        # Start-Sleep 1
         # Stop-Process -processname "conhost"
 
         exit 0 # success
