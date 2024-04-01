ce57e05cfb4d64f3623e18367847679b29cde2e8
Schalk Burger <schalkb@gmail.com>
2024-04-01 18:26:29 +0200

    Update launcher for 4x3

--- launchers/cs2-launcher.ps1
+++ launchers/cs2-launcher.ps1
@@ -14,27 +14,27 @@
 try {
         clear-host
         ""
-        # Write-Host "Press any key to derank...";
-        # $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
+        Write-Host "Press any key to derank...";
+        $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
         # Start CSGO
-        # SetScreenResolution 1920 1080
-        # Start-Sleep 2
+        SetScreenResolution 1280 960
+        Start-Sleep 2
         Start-Process steam://launch/730/Dialog
         # New-BurntToastNotification -AppLogo "E:\Apps\Windows Apps\Custom Icons\cs2.png" -Text "Launching Counter-Strike 2...", 'Counter-Strike 2'
         # MultiMonitorTool /LoadConfig "E:\GitHub\cs-configs\launchers\monitors-cs2-1280x960.cfg"
-        # Write-Host "`n";
+        # Write-Host "`n";U
         # Notepad wait
-        # Start-Process -WindowStyle "Minimized" -Wait notepad
+        Start-Process -WindowStyle "Minimized" -Wait notepad
         # Start-Process -WindowStyle "Minimized" -Wait "C:\Program Files\WindowsApps\Microsoft.WindowsAlarms_11.2309.5.0_x64__8wekyb3d8bbwe\Time.exe"
         # Stop CSGO
-        # Stop-Process -processname "cs2"
-        # SetScreenResolution 1920 1080
-        # Start-Sleep 2
+        Stop-Process -processname "cs2"
+        SetScreenResolution 1920 1080
+        Start-Sleep 2
         # MultiMonitorTool /LoadConfig "E:\Apps\Windows Apps\MultiMonitorTool\monitors-default.cfg"
         # New-BurntToastNotification -AppLogo "E:\Apps\Windows Apps\Custom Icons\cs2.png" -Text "Stopped playing CS2.", 'Counter-Strike 2'
         # Close terminal
-        # Start-Sleep 1
-        # Stop-Process -processname "conhost"
+        Start-Sleep 1
+        Stop-Process -processname "conhost"
 
         exit 0 # success
 }
