3a24ab53b2f16c721e6362f529eb6099b302de34
Schalk Burger <schalkb@gmail.com>
2024-05-09 20:04:07 +0200

    Update video settings

--- launchers/cs2-launcher.ps1
+++ launchers/cs2-launcher.ps1
@@ -16,22 +16,23 @@ try {
         ""
         Write-Host "Press any key to derank...";
         $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
+
+        # Set width and height of the terminal window
+        [System.Console]::SetWindowSize(70, 20)
+
+        # Set the title of the terminal window
+        [System.Console]::Title = "CS2 Launcher"
+
         # Start CSGO
         SetScreenResolution 1280 960
         Start-Sleep 2
         Start-Process steam://launch/730/Dialog
-        # New-BurntToastNotification -AppLogo "E:\Apps\Windows Apps\Custom Icons\cs2.png" -Text "Launching Counter-Strike 2...", 'Counter-Strike 2'
-        # MultiMonitorTool /LoadConfig "E:\GitHub\cs-configs\launchers\monitors-cs2-1280x960.cfg"
-        # Write-Host "`n";U
         # Notepad wait
         Start-Process -WindowStyle "Minimized" -Wait notepad
-        # Start-Process -WindowStyle "Minimized" -Wait "C:\Program Files\WindowsApps\Microsoft.WindowsAlarms_11.2309.5.0_x64__8wekyb3d8bbwe\Time.exe"
         # Stop CSGO
         Stop-Process -processname "cs2"
         SetScreenResolution 1920 1080
         Start-Sleep 2
-        # MultiMonitorTool /LoadConfig "E:\Apps\Windows Apps\MultiMonitorTool\monitors-default.cfg"
-        # New-BurntToastNotification -AppLogo "E:\Apps\Windows Apps\Custom Icons\cs2.png" -Text "Stopped playing CS2.", 'Counter-Strike 2'
         # Close terminal
         Start-Sleep 1
         Stop-Process -processname "conhost"
