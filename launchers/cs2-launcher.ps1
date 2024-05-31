450efa1cce40b5c7d47d2ae8572ed2f01be5d91c
Schalk Burger <schalkb@gmail.com>
2024-05-31 08:59:50 +0200

    Sens and crosshair updates

--- launchers/cs2-launcher.ps1
+++ launchers/cs2-launcher.ps1
@@ -1,45 +1,64 @@
 <#
 .SYNOPSIS
-	Launches the Windows Terminal app
+    Launches Counter-Strike 2 through Windows Terminal
 .DESCRIPTION
-	This script launches the Windows Terminal application.
+    This script launches the Windows Terminal, starts Counter-Strike 2, waits for Notepad to close,
+    and then stops Counter-Strike 2 and closes the terminal window.
 .EXAMPLE
-	PS> ./open-windows-terminal
+    PS> ./cs2-launcher.ps1
 .LINK
-	https://github.com/fleschutz/PowerShell
+    https://github.com/schalkburger
 .NOTES
-	Author: Markus Fleschutz | License: CC0
+    Author: Schalk Burger | License: CC0
 #>
 
 try {
-        clear-host
-        ""
-        Write-Host "Press any key to derank...";
-        $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
+    # Load the necessary Win32 API functions
+    Add-Type @"
+        using System;
+        using System.Runtime.InteropServices;
+        public class Win32 {
+            [DllImport("user32.dll")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        }
+"@
 
-        # Set width and height of the terminal window
-        [System.Console]::SetWindowSize(70, 20)
+    # Get the first Windows Terminal process
+    $terminalProcess = Get-Process -Name WindowsTerminal | Select-Object -First 1
+    if (-not $terminalProcess) {
+        throw "Windows Terminal process not found."
+    }
 
-        # Set the title of the terminal window
-        [System.Console]::Title = "CS2 Launcher"
+    $terminalWindowHandle = $terminalProcess.MainWindowHandle
 
-        # Start CSGO
-        SetScreenResolution 1280 960
-        Start-Sleep 2
-        Start-Process steam://launch/730/Dialog
-        # Notepad wait
-        Start-Process -WindowStyle "Minimized" -Wait notepad
-        # Stop CSGO
-        Stop-Process -processname "cs2"
-        SetScreenResolution 1920 1080
-        Start-Sleep 2
-        # Close terminal
-        Start-Sleep 1
-        Stop-Process -processname "conhost"
+    Clear-Host
+    Write-Host "Press any key to continue..."
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 
-        exit 0 # success
+    # Minimize the terminal window
+    [void][Win32]::ShowWindow($terminalWindowHandle, 6) # 6 represents SW_MINIMIZE
+
+    # Set the title of the terminal window
+    [System.Console]::Title = "CS2 Launcher"
+
+    # Start Counter-Strike 2
+    & SetScreenResolution 1280 960
+    Start-Sleep -Seconds 2
+    Start-Process "steam://launch/730/Dialog"
+    # Notepad wait
+    Start-Process -FilePath "notepad" -WindowStyle Minimized -Wait
+    # Stop Counter-Strike 2
+    Stop-Process -Name "cs2"
+    & SetScreenResolution 1920 1080
+    Start-Sleep -Seconds 2
+    # Close terminal
+    Start-Sleep -Seconds 1
+    Stop-Process -Name "WindowsTerminal"
+
+    return 0 # success
 }
 catch {
-        "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
-        exit 1
+    Write-Host "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message)"
+    return 1
 }
